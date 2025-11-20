e09370081c70ded7ef28de056b4144cc9e7c72eb
Xi Wu <youngjoycewu@gmail.com>
2025-11-20 21:44:41 +0000

    add helper function getLetterPool() for wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,31 @@
+export const LETTER_DISTRIBUTION = Object.freeze({
+  A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2,
+  I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2,
+  Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1,
+  Y: 2, Z: 1,
+});
+
+const getLetterPool = (distribution = LETTER_DISTRIBUTION) => {
+  const pool = [];
+  Object.entries(distribution).forEach(([letter, quantity]) => {
+    for (let i = 0; i < quantity; i++) {
+      pool.push(letter);
+    }
+  });
+  return pool;
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const pool = getLetterPool();
+
+  const hand = [];
+  for (let i = 0; i < 10; i++) {
+    const idx = Math.floor(Math.random() * pool.length);
+    hand.push(pool[idx]);
+    pool.splice(idx, 1); // remove drawn letter so it can't be drawn again
+  }
+
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
