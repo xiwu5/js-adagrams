25f36f4dda96405dccd1b2135c1502a77f79d012
Xi Wu <youngjoycewu@gmail.com>
2025-11-21 11:14:13 +0000

    add a frozen SCORE_CHART constant

--- src/adagrams.js
+++ src/adagrams.js
@@ -4,6 +4,13 @@ export const LETTER_DISTRIBUTION = Object.freeze({
   Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1,
   Y: 2, Z: 1,
 });
+
+export const SCORE_CHART = Object.freeze({
+  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4,
+  I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3,
+  Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
+  Y: 4, Z: 10,
+});
 const NUM_TILES_ALLOWED_IN_HAND = 10;
 
 const getLetterPool = (distribution = LETTER_DISTRIBUTION) => {
